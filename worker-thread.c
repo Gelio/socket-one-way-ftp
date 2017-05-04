16b145492ae38f4140fdaff8b908bda11d59a66c
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-05-04 12:42:18 +0200

    add server logic - redirecting input to a file

--- worker-thread.c
+++ worker-thread.c
@@ -1,6 +1,13 @@
 #include "common.h"
 #include "worker-thread.h"
 
+#include <arpa/inet.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#define BUFFER_SIZE 400
+#define MAX_FILE_NAME 100
+
 void setWorkerThreadSignalHandling(sigset_t *previousMask)
 {
     sigset_t blockMask = prepareBlockMask();
@@ -26,11 +33,51 @@ void *workerThread(void *args)
     printf("[%d] started\n", workerArgs->id);
 
     clientNode_t *client = safePopClientFromQueue(workerArgs->clientQueue, workerArgs->clientQueueMutex);
+    char *clientAddrReadable = inet_ntoa(client->clientAddr.sin_addr);
+    if (clientAddrReadable == NULL)
+    {
+        printf("[%d] cannot determine clients IP", workerArgs->id);
+        clientAddrReadable = "unknown";
+    }
+
+    printf("[%d] client connected from %s (port %d)\n", workerArgs->id, clientAddrReadable, ntohs(client->clientAddr.sin_port));
+    char fileName[MAX_FILE_NAME];
+    snprintf(fileName, MAX_FILE_NAME, "%s_%d.out", clientAddrReadable, ntohs(client->clientAddr.sin_port));
+    int fileDes = open(fileName, O_WRONLY | O_CREAT | O_APPEND, 0644);
+    if (fileDes < 0)
+        printf("[%d] cannot open file %s\n", workerArgs->id, fileName);
+    else
+    {
+        char buffer[BUFFER_SIZE];
+        while (1)
+        {
+            ssize_t bytesRead = bulkRead(client->clientSocket, buffer, BUFFER_SIZE);
+            if (bytesRead < 0)
+            {
+                printf("[%d] read error\n", workerArgs->id);
+                perror("read error");
+                break;
+            }
+            else if (bytesRead == 0)
+                break;
+
+            ssize_t bytesWritten = bulkWrite(fileDes, buffer, bytesRead);
+            if (bytesWritten <= 0)
+            {
+                printf("[%d] write error\n", workerArgs->id);
+                perror("write error");
+                break;
+            }
+        }
+    }
+
 
     printf("[%d] closing connection to the client\n", workerArgs->id);
     if (TEMP_FAILURE_RETRY(close(client->clientSocket)) < 0)
-        ERR("close");
+        ERR("close client socket");
     free(client);
+    if (fileDes >= 0 && TEMP_FAILURE_RETRY(close(fileDes)) < 0)
+        ERR("close file descriptor");
 
     int threadId = workerArgs->id;
     workerThreadCleanup(workerArgs, &previousMask);
